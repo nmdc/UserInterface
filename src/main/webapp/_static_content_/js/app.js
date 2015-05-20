8ceb3913615a55cde8c86b49cc2cc27625960b08
ingekr <inge.eliassen@cmr.no>
2015-05-20 15:26:28 +0200

    Geographical filtering

--- src/main/webapp/_static_content_/js/app.js
+++ src/main/webapp/_static_content_/js/app.js
@@ -28,10 +28,16 @@
                 facets: [],
                 search: {
                     response: {}, itemsPerPage: 10, currentPage: 0, text: '', coverage: {
-                        useGeographicalCoverage: false,
-                        beginDate: Util.formatDate(new Date(1800, 0, 1)),
-                        endDate: Util.formatDate(new Date()),
-                        useTemporalCoverage: false
+                        geographical: {
+                            use: false,
+                            type: 'mapBoundingBox',
+                            coordinates: []
+                        },
+                        temporal: {
+                            use: false,
+                            beginDate: Util.formatDate(new Date(1800, 0, 1)),
+                            endDate: Util.formatDate(new Date())
+                        }
                     }
                 },
                 basket: {count: 0, idToItem: {}},
@@ -109,6 +115,13 @@
                     if (Model.search.text) {
                         terms.push('"' + Model.search.text + '"');
                     }
+                    if (Model.search.coverage.geographical.use && Model.search.coverage.geographical.coordinates.length > 0) {
+                        var coordinates = [];
+                        Model.search.coverage.geographical.coordinates.forEach(function (point) { coordinates.push(point.lng + ' ' + point.lat); });
+                        var first = Model.search.coverage.geographical.coordinates[0];
+                        coordinates.push(first.lng + ' ' + first.lat);
+                        terms.push('location_rpt:"IsWithin(POLYGON((' + coordinates.join(',') + ')))"');
+                    }
                     return terms.join(' AND ');
                 }
 
@@ -127,24 +140,105 @@
                 if (newValue === oldValue) return;
                 ctrl.search(true);
             });
-            $scope.$watchCollection('ctrl.model.search.coverage', function (newValue, oldValue) {
-                if (newValue === oldValue) return;
+            $scope.$watchCollection('ctrl.model.search.coverage.geographical', function (newValue, oldValue) {
+                if (newValue === oldValue || !newValue.use && !oldValue.use) return;
+                var newUse = newValue.use && newValue.coordinates.length > 0;
+                var oldUse = oldValue.use && oldValue.coordinates.length > 0;
+                if (!newUse && !oldUse) return;
                 ctrl.search();
             });
-
+            $scope.$watchCollection('ctrl.model.search.coverage.temporal', function (newValue, oldValue) {
+                if (newValue === oldValue) return;
+                if (!newValue.use && !oldValue.use) return;
+                ctrl.search();
+            });
             $scope.$on('$destroy', cancelSearch);
         }])
         .directive('nmdcMap', ['NmdcModel', function (Model) {
             return {
                 restrict: 'A',
                 link: function (scope, element, attrs) {
-                    var map = new L.Map(attrs.id, Model.map.options);
-                    map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '© OpenStreetMap contributors'}));
+                    var map = L.map(attrs.id, Model.map.options);
+                    map.addLayer(L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '© OpenStreetMap contributors'}));
 
-                    scope.$on('$destroy', function () {
+                    var itemGroup = L.featureGroup();
+                    map.addLayer(itemGroup);
+
+                    var drawerOptions = {shapeOptions: {color: '#03f'}};
+                    var rectangleDrawer = new L.Draw.Rectangle(map, drawerOptions);
+                    var polygonDrawer = new L.Draw.Polygon(map, drawerOptions);
+
+                    var DrawControl = L.Control.extend({
+                        options: {position: 'topleft'},
+                        onAdd: function (map) {
+                            var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-draw-section leaflet-draw-toolbar');
+
+                            var boundingBox = L.DomUtil.create('a', 'nmdc-map-button nmdc-map-button-bounding-box', container);
+                            boundingBox.title = 'Use map bounding box';
+                            boundingBox.text = 'Box';
+                            boundingBox.onclick = function () { setType('mapBoundingBox', null); };
+
+                            var rectangle = L.DomUtil.create('a', 'nmdc-map-button leaflet-draw-draw-rectangle', container);
+                            rectangle.title = 'Use a rectangle';
+                            rectangle.onclick = function () { setType('rectangle', rectangleDrawer); };
+
+                            var polygon = L.DomUtil.create('a', 'nmdc-map-button leaflet-draw-draw-polygon', container);
+                            polygon.title = 'Use a polygon';
+                            polygon.onclick = function () { setType('polygon', polygonDrawer); };
+
+                            return container;
+                        }
+                    });
+                    map.addControl(new DrawControl());
+
+                    map.on('moveend', function () {
                         Model.map.options.center = map.getCenter();
                         Model.map.options.zoom = map.getZoom();
+                        if (Model.search.coverage.geographical.type == 'mapBoundingBox') {
+                            setCoordinates(L.rectangle(map.getBounds()).getLatLngs());
+                        }
                     });
+
+                    map.on('draw:created', function (e) {
+                        var layer = e.layer;
+                        addItem(layer);
+                        setCoordinates(layer.getLatLngs());
+                    });
+
+                    function addItem(layer) {
+                        itemGroup.addLayer(layer);
+                        layer.editing.enable();
+                        layer.on('edit', function () {
+                            setCoordinates(layer.getLatLngs());
+                        });
+                    }
+
+                    function setType(type, drawer) {
+                        scope.$apply(function () {
+                            Model.search.coverage.geographical.type = type;
+                            Model.search.coverage.geographical.coordinates = [];
+                        });
+                        itemGroup.clearLayers();
+                        rectangleDrawer.disable();
+                        polygonDrawer.disable();
+                        if (drawer) drawer.enable();
+                    }
+
+                    function setCoordinates(coordinates) {
+                        scope.$apply(function () {
+                            Model.search.coverage.geographical.coordinates = coordinates.slice();
+                        });
+                    }
+
+                    function init() {
+                        var coordinates = Model.search.coverage.geographical.coordinates;
+                        var type = Model.search.coverage.geographical.type;
+                        if (type == 'rectangle') addItem(L.rectangle(coordinates));
+                        if (type == 'polygon') addItem(L.polygon(coordinates));
+                        if (type == 'mapBoundingBox') Model.search.coverage.geographical.coordinates = L.rectangle(map.getBounds()).getLatLngs();
+                    }
+
+                    init();
                 }
             };
         }])
